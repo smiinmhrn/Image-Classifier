b22df389f3e3b5a4583da16b3799ac3f39d08321
Samin <samin.mhr2004@gmail.com>
2023-09-08 13:30:12 +0330

    .hpp file updated

--- process/process.hpp
+++ process/process.hpp
@@ -27,4 +27,16 @@ float math_matrix_average(float matrix[IMAGE_SIZE][IMAGE_SIZE],
 float math_matrix_standard_deviation(float matrix[IMAGE_SIZE][IMAGE_SIZE],
                                      int x_start, int x_end, int y_start, int y_end, float avrg);
 
+void act_of_training(FEATURES_LABELS &data);
+
+void act_of_extracting_features(float matrix[IMAGE_SIZE][IMAGE_SIZE],
+                                float save_features[]);
+
+float act_of_distance_two_arrays(float first_array[], float second_array[], int continu);
+
+float (float array[], float matrix[][NUMBER_OF_FEATURES],
+                                       float distance[]);
+
+void training(FEATURES_LABELS &data);
+
 #endif
